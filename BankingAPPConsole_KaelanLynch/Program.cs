5d8eb45185d44a8d68488ce0a11ab7e7dac42c54
kaelan87 <kaelanlynch@yahoo.com>
2022-03-09 15:19:21 -0500

    Project 0 Initialized !!

---
+++ BankingAPPConsole_KaelanLynch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingAPPConsole_KaelanLynch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
